58f6dbcf0485f0941087e30ad0f4738b1def3865
Liu <lyue@ad.unc.edu>
2024-08-26 15:44:46 -0400

    adding additional parameter comments, adding dictionary to dataframe transformation

--- tabularepimdl/BirthProcess.py
+++ tabularepimdl/BirthProcess.py
@@ -14,12 +14,13 @@ class BirthProcess(Rule):
 
         @param rate the rate at which births happen per time step (N*rate births).
         @param state_start_sig the column values that newly borne individuals should have.
+        @param stochastic, is this rule stochastic process
         """
 
         super().__init__()
         self.rate = rate
 
-        #is their a better way to do this? #yl: what is the input of start_state_sig and what better is expected?
+        #is their a better way to do this?
         if isinstance(start_state_sig, dict):
             #start_state_sig = pd.DataFrame(start_state_sig,index=[0])
             self.start_state_sig = pd.DataFrame([start_state_sig]) #put dict to a list to keep index = 0 condition i.e. single row
@@ -32,6 +33,10 @@ class BirthProcess(Rule):
         self.stochastic = stochastic
 
     def get_deltas(self, current_state, dt = 1.0, stochastic =None):
+        """
+        @param current_state, a data frame (at the moment) w/ the current epidemic state
+        @param dt, the size of the timestep
+        """
         if stochastic is None:
             stochastic = self.stochastic
         
