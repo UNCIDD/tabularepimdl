fb991d86225f380549b5e3b827a3e69701cc17d9
Liu <lyue@ad.unc.edu>
2024-09-13 16:15:37 -0400

    add categorical conversion, add debug code, additional questions are left in-line

--- tabularepimdl/WAIFWTransmission.py
+++ tabularepimdl/WAIFWTransmission.py
@@ -38,27 +38,38 @@ class WAIFWTransmission(Rule):
         if stochastic is None:
             stochastic = self.stochastic
 
-        ##create an array of the number of infections in each group.
-        inf_array = current_state.loc[current_state[self.inf_col]==self.i_st].groupby(self.group_col)['N'].sum(numeric_only=True).values #moved ['N'] position 
+        #question: since group_col needs to be categorical type, do we want to convert current_state[group_col] to pd.categorical in this rule? 
+        #Instead of doing the conversion in model applications such as AgingPopulation?
 
-        #print(isinstance(current_state[self.group_col].dtype, pd.CategoricalDtype)) #is_categorical_dtype is deprecated, replaced with the isinstance function
+        #quesiton and fix: convert group_col to categorical type first, so groupby observed=False generate full list of array values
+        current_state[self.group_col]=pd.Categorical(current_state[self.group_col])
+
+        ##create an array for the total number of infections in each unique group. Only records with i_st are sumed, other records's N are filled with 0.
+        inf_array = current_state.loc[current_state[self.inf_col]==self.i_st].groupby(self.group_col, observed=False)['N'].sum(numeric_only=True).values #moved ['N'] position 
+
+        print('is it category?', isinstance(current_state[self.group_col].dtype, pd.CategoricalDtype)) #is_categorical_dtype is deprecated, replaced with the isinstance function
         #print(current_state.loc[current_state[self.inf_col]==self.i_st].groupby(self.group_col).sum(numeric_only=True)) #debug
         #print("))))") #debug
-        #print(inf_array) #debug
+        print('inf_array is', inf_array) #debug
 
-        #get the probability of being infected in each group
+        #get the probability of being infected in each unique group
         prI = np.power(np.exp(-dt*self.waifw_matrix),inf_array)
+        print('powered prI is', prI) #debug
         prI = 1-prI.prod(axis=1)
+        print('1-prI prod', prI) #debug
 
-        ##get folks in susceptible states
+        ##get folks in susceptible states which link to all unique groups
         deltas = current_state.loc[current_state[self.inf_col]==self.s_st]
+        print('deltas is', deltas, '\n') #debug
+        print('prI codes are', prI[deltas[self.group_col].cat.codes], '\n') #debug
 
-        ##do infectious process
+        ##do infectious process, getting the number of individuals who get infected from susceptible status
         if not stochastic:
             deltas = deltas.assign(N=-deltas['N']*prI[deltas[self.group_col].cat.codes])
         else:
             deltas = deltas.assign(N=-np.random.binomial(deltas['N'],prI[deltas[self.group_col].cat.codes]))
 
+        print('deltas after infection process:', deltas)
         deltas_add = deltas.assign(N=-deltas['N'])
         deltas_add[self.inf_col] = self.inf_to
 
