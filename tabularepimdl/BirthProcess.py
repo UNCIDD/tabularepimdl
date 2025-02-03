a69a5fa22f148462c73cbdc89091ffa281508a9f
Liu <lyue@ad.unc.edu>
2025-02-03 16:42:38 -0500

    add error handling, improve code robustness, improve comments

--- tabularepimdl/BirthProcess.py
+++ tabularepimdl/BirthProcess.py
@@ -8,19 +8,18 @@ class BirthProcess(Rule):
     Represents a birth process where people are borne based
     on a birth rate based on the full poplation size."""
 
-    def __init__(self, rate:float, start_state_sig, stochastic=False)-> None:
+    def __init__(self, rate: float, start_state_sig, stochastic=False)-> None:
         """!
-        Initializes the rule.
+        Initialization.
 
-        @param rate the rate at which births happen per time step (N*rate births).
-        @param state_start_sig the column values that newly borne individuals should have.
-        @param stochastic, is this rule stochastic process
+        @param rate: birth rate at per time step (where N*rate births occur).
+        @param state_start_sig: initial state configuration for new births.
+        @param stochastic: whether the transition is stochastic or deterministic.
         """
 
         super().__init__()
         self.rate = rate
 
-        #is their a better way to do this?
         if isinstance(start_state_sig, dict):
             self.start_state_sig = pd.DataFrame([start_state_sig]) #convert a dict to a dataframe
         elif isinstance(start_state_sig, pd.DataFrame):
@@ -28,25 +27,28 @@ class BirthProcess(Rule):
         else:
             raise ValueError ("start_state_sig must be either a dictionary or a pandas DataFrame")
 
-        #self.start_state_sig = start_state_sig
         self.stochastic = stochastic
 
-    def get_deltas(self, current_state, dt = 1.0, stochastic =None):
+    def get_deltas(self, current_state: pd.DataFrame, dt=1.0, stochastic=None):
         """
-        @param current_state, a data frame (at the moment) w/ the current epidemic state
-        @param dt, the size of the timestep
+        @param current_state: a dataframe (at the moment) representing the current epidemic state. Must include column 'N'.
+        @param dt: size of the timestep.
         """
+        required_columns = {"N"} #check if column N presents in current_state
+        if not required_columns.issubset(current_state.columns):
+            raise ValueError(f"Missing required columns in current_state: {required_columns}")
+        
         if stochastic is None:
             stochastic = self.stochastic
         
-        ##Just returns a delta with the start
-        ##state signature and the right number of
-        ##births.
-        N = current_state['N'].sum() #check AgingPopulation module for format of current_state.
-        if not stochastic: #False condition: deterministic process
-            births = self.start_state_sig.assign(N=N*(1-np.exp(-dt*self.rate)))
-        else: #True condition: stochastic process
-            births = self.start_state_sig.assign(N=np.random.poisson(N*(1-np.exp(-dt*self.rate))))  #get a random single outcome value by using poisson distribution
+        #returns a delta with the start_state_signature and the right number of births.
+        N = current_state["N"].sum()
+        births = self.start_state_sig.copy()
+        birth_value = N * (1-np.exp(-dt*self.rate))
+        if not stochastic:
+            births["N"] = birth_value
+        else:
+            births["N"] = np.random.poisson(birth_value)  #get a random single outcome value by poisson distribution
         return births
         
     def to_yaml(self):
