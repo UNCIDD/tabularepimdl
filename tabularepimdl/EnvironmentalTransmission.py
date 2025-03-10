7015c22e042b177ab9e1fa6c5545ab4504c5045e
Liu <lyue@AD.UNC.EDU>
2025-03-10 11:53:14 -0400

    update data validation with pydantic

--- tabularepimdl/EnvironmentalTransmission.py
+++ tabularepimdl/EnvironmentalTransmission.py
@@ -1,30 +1,28 @@
 import pandas as pd
 import numpy as np
 from tabularepimdl.Rule import Rule
+from pydantic import BaseModel, Field
+from typing import Annotated
 
+class EnvironmentalTransmission(Rule, BaseModel):
 
-class EnvironmentalTransmission(Rule):
-
-    def __init__(self, in_beta: float, inf_col: str, trait_col: str, s_st: str = "S", i_st: str = "I", inf_to: str = "I", stochastic: bool = False) -> None:
-        '''!
-        Initialization.
-
-        @param in_beta: transmission risk if trait shared.
-        @param inf_col: the column designating infection state.
-        @param trait_col: the column designaing the trait.
-        @param s_st: the state for susceptibles, assumed to be S
-        @param i_st: the state for infectious, assumed to be I
-        @param inf_to: the state infectious folks go to, assumed to be I.
-        @param stochastic: whether the process is stochastic or deterministic.
-        '''
-        super().__init__()
-        self.in_beta = in_beta
-        self.inf_col = inf_col
-        self.trait_col = trait_col
-        self.s_st = s_st
-        self.i_st = i_st
-        self.inf_to = inf_to
-        self.stochastic = stochastic
+    #def __init__(self, in_beta: float, inf_col: str, trait_col: str, s_st: str = "S", i_st: str = "I", inf_to: str = "I", stochastic: bool = False) -> None:
+    '''!Initialization.
+    @param in_beta: transmission risk if trait shared.
+    @param inf_col: the column designating infection state.
+    @param trait_col: the column designaing the trait.
+    @param s_st: the state for susceptibles, assumed to be S
+    @param i_st: the state for infectious, assumed to be I
+    @param inf_to: the state infectious folks go to, assumed to be I.
+    @param stochastic: whether the process is stochastic or deterministic.
+    '''
+    in_beta: Annotated[int | float, Field(ge=0)]
+    inf_col: str
+    trait_col:str
+    s_st: str = "S"
+    i_st: str = "I"
+    inf_to: str = "I"
+    stochastic: bool = False
         
     def get_deltas(self, current_state: pd.DataFrame, dt: int | float =1.0, stochastic: bool = None) -> pd.DataFrame:
         """
@@ -51,20 +49,19 @@ class EnvironmentalTransmission(Rule):
         # Vectorized calculation of prI
         deltas["prI"] = 1 - np.exp(-dt*self.in_beta)
             
-        # Update N values based on prI
+        # Update N values based on prI, folks out of S
         if not stochastic:
-            deltas["N"] = deltas["N"] * deltas["prI"]    
+            deltas["N"] = -deltas["N"] * deltas["prI"]    
         else:
-            deltas["N"] = np.random.binomial(deltas["N"], deltas["prI"])
+            deltas["N"] = -np.random.binomial(deltas["N"], deltas["prI"])
         
         #drop temporary columns inI, outI, prI
         deltas.drop(["prI"], axis=1, inplace=True)
         
-        # Update deltas and deltas_add DataFrames
-        deltas["N"] = -deltas["N"] #folks out of S
-        deltas_add["N"] = -deltas["N"]
-        
-        deltas_add[self.inf_col] = self.inf_to #folks into I
+        #Update deltas_add DataFrames, folks into I
+        deltas_add = deltas_add.assign(**{self.inf_col: self.inf_to, "N": -deltas["N"]})
+        #deltas_add["N"] = -deltas["N"]
+        #deltas_add[self.inf_col] = self.inf_to 
 
         rc = pd.concat([deltas,deltas_add])
         #print('ST combined deltas are\n', rc) #debug
@@ -72,15 +69,6 @@ class EnvironmentalTransmission(Rule):
     
     def to_yaml(self) -> dict:
         rc = {
-            'tabularepimdl.SharedTraitInfection': {
-                'in_beta': self.in_beta,
-                'out_beta': self.out_beta,
-                'inf_col': self.inf_col,
-                'trait_col': self.trait_col,
-                's_st': self.s_st,
-                'i_st': self.i_st,
-                'inf_to': self.inf_to,
-                'stochastic': self.stochastic
-            }
+            'tabularepimdl.EnvironmentalTransmission': self.model_dump()
         }
         return rc
