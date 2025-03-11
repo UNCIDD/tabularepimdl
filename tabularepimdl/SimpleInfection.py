240cdb4535f614fcc13b88971be95e4c81496bf7
Liu <lyue@AD.UNC.EDU>
2025-03-11 16:51:52 -0400

    data validation with pydantic

--- tabularepimdl/SimpleInfection.py
+++ tabularepimdl/SimpleInfection.py
@@ -1,34 +1,32 @@
 from tabularepimdl.Rule import Rule
 import numpy as np
 import pandas as pd
+from pydantic import BaseModel, Field
+from typing import Annotated
 
-class SimpleInfection(Rule):
-    """! 
-    Represents a simple infection process where people in one column are infected by people
+class SimpleInfection(Rule, BaseModel):
+    """!Class represents a simple infection process where people in one column are infected by people
     in a given state in that same column with a probability."""
 
-    def __init__(self, beta:float, column, s_st="S", i_st="I", inf_to="I", freq_dep=True, stochastic=False) -> None:
-        """!
-        Initialization. 
+    #def __init__(self, beta:float, column, s_st="S", i_st="I", inf_to="I", freq_dep=True, stochastic=False) -> None:
+    """!Initialization. 
+    @param beta: the transmission parameter. 
+    @param column: name of the column this rule applies to.
+    @param s_st: the state for susceptibles, assumed to be S.
+    @param i_st: the state for infectious, assumed to be I.
+    @param inf_to: the state infectious folks go to, assumed to be I.
+    @param freq_dep: whether this model is a frequency dependent model.
+    @param stochastic: whether the transition is stochastic or deterministic.
+    """
+    beta: Annotated[int | float, Field(ge=0)]
+    column: str
+    s_st: str = "S"
+    i_st: str = "I"
+    inf_to: str = "I"
+    freq_dep: bool = True
+    stochastic: bool = False
 
-        @param beta: the transmission parameter. 
-        @param column: name of the column this rule applies to.
-        @param s_st: the state for susceptibles, assumed to be S.
-        @param i_st: the state for infectious, assumed to be I.
-        @param inf_to: the state infectious folks go to, assumed to be I.
-        @param freq_dep: whether this model is a frequency dependent model.
-        @param stochastic: whether the transition is stochastic or deterministic.
-        """
-        super().__init__() 
-        self.beta = beta
-        self.column = column
-        self.s_st = s_st
-        self.i_st = i_st
-        self.inf_to = inf_to
-        self.freq_dep = freq_dep
-        self.stochastic = stochastic
-
-    def get_deltas(self, current_state: pd.DataFrame, dt=1.0, stochastic=None):
+    def get_deltas(self, current_state: pd.DataFrame, dt: int | float =1.0, stochastic: bool = None) -> pd.DataFrame:
         """
         @param current_state: a dataframe (at the moment) representing the current epidemic state. Must include column 'N'.
         @param dt: size of the timestep.
@@ -42,39 +40,30 @@ class SimpleInfection(Rule):
 
         if total_population != 0:
             if self.freq_dep:
-                beta = self.beta/(current_state['N'].sum())
+                beta = self.beta/total_population
             else:
                 beta = self.beta
         else:
             beta = self.beta
 
-        infectious = current_state.loc[current_state[self.column]==self.i_st, 'N'].sum()
+        infectious = current_state.loc[current_state[self.column]==self.i_st, "N"].sum()
 
         deltas = current_state.loc[current_state[self.column]==self.s_st].copy()
 
         if not stochastic:
-            deltas["N"] = -deltas["N"]*(1 - np.power(np.exp(-dt*beta), infectious))
+            deltas["N"] = -deltas["N"] * (1 - np.power(np.exp(-dt*beta), infectious))
         else:
             deltas["N"] = -np.random.binomial(deltas["N"], 1 - np.power(np.exp(-dt*beta),infectious))
         
-        tmp = deltas.copy()
-        tmp["N"] = -deltas["N"]
-        tmp[self.column] = self.inf_to
-
-        return pd.concat([deltas, tmp]).reset_index(drop=True)
+        deltas_add = deltas.copy()
+        deltas_add = deltas_add.assign(**{self.column: self.inf_to, "N": -deltas["N"]})
+        
+        return pd.concat([deltas, deltas_add]).reset_index(drop=True)
         
         
     def to_yaml(self):
         rc = {
-            'tabularepimdl.SimpleInfection': {
-                'beta': self.beta,
-                'column': self.column,
-                's_st': self.s_st,
-                'i_st': self.i_st,
-                'inf_to': self.inf_to,
-                'freq_dep':self.freq_dep,
-                'stochastic':self.stochastic
-            }
+            'tabularepimdl.SimpleInfection': self.model_dump()
         }
 
         return rc        
