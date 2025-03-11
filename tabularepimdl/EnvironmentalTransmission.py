2203336b9c942c6eb8a7f7dfb88e2990496d0d41
Liu <lyue@AD.UNC.EDU>
2025-03-10 20:19:23 -0400

    data validation with pydantic

--- tabularepimdl/EnvironmentalTransmission.py
+++ tabularepimdl/EnvironmentalTransmission.py
@@ -5,10 +5,11 @@ from pydantic import BaseModel, Field
 from typing import Annotated
 
 class EnvironmentalTransmission(Rule, BaseModel):
-
-    #def __init__(self, in_beta: float, inf_col: str, trait_col: str, s_st: str = "S", i_st: str = "I", inf_to: str = "I", stochastic: bool = False) -> None:
+    """!Class represents disease transmission that happens in nature environment without any human-to-human or cross-species infection.
+    """
+    #def __init__(self, beta: float, inf_col: str, trait_col: str, s_st: str = "S", i_st: str = "I", inf_to: str = "I", stochastic: bool = False) -> None:
     '''!Initialization.
-    @param in_beta: transmission risk if trait shared.
+    @param beta: transmission risk if trait shared.
     @param inf_col: the column designating infection state.
     @param trait_col: the column designaing the trait.
     @param s_st: the state for susceptibles, assumed to be S
@@ -16,7 +17,7 @@ class EnvironmentalTransmission(Rule, BaseModel):
     @param inf_to: the state infectious folks go to, assumed to be I.
     @param stochastic: whether the process is stochastic or deterministic.
     '''
-    in_beta: Annotated[int | float, Field(ge=0)]
+    beta: Annotated[int | float, Field(ge=0)]
     inf_col: str
     trait_col:str
     s_st: str = "S"
@@ -47,7 +48,7 @@ class EnvironmentalTransmission(Rule, BaseModel):
 
        
         # Vectorized calculation of prI
-        deltas["prI"] = 1 - np.exp(-dt*self.in_beta)
+        deltas["prI"] = 1 - np.exp(-dt*self.beta)
             
         # Update N values based on prI, folks out of S
         if not stochastic:
@@ -64,7 +65,7 @@ class EnvironmentalTransmission(Rule, BaseModel):
         #deltas_add[self.inf_col] = self.inf_to 
 
         rc = pd.concat([deltas,deltas_add])
-        #print('ST combined deltas are\n', rc) #debug
+        print('env combined deltas are\n', rc) #debug
         return rc.loc[rc["N"]!=0].reset_index(drop=True) #reset index for the new dataframe
     
     def to_yaml(self) -> dict:
