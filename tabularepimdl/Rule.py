fe60af814eddefaefcacabf377e073c10a81a3e2
Liu <lyue@AD.UNC.EDU>
2025-03-21 12:27:13 -0400

    data type and comments improvement

--- tabularepimdl/Rule.py
+++ tabularepimdl/Rule.py
@@ -1,26 +1,24 @@
-## Define a class that represents a transition rule. It its simplest form a rule just takes in the current state,
+## Define a class that represents a transition rule. Its simplest form is a rule just takes in the current state,
 ## and returns as set of deltas that will be applied to the appropriate parts of the current state. These deltas
-## should specify the full row signature. 
-from abc import ABC, abstractmethod  #the class "abstractclassmethod" is deprecated, use classmethod with abstractmethod()
+## should specify the full row signature.
+from abc import ABC, abstractmethod
 import inspect
 import importlib
+import pandas as pd
 
 class Rule(ABC):
 
     stochastic: bool
     """
-    @param stochastic, is a rule stochastic process
+    @param stochastic: whether the process is stochastic or deterministic.
     """
         
     @abstractmethod
-    def get_deltas(self, current_state, dt:float, stochastic):
-        """! Method should take in current state and return a 
-        series of deltas to that state
-
-        @param current_state a data frame (at the moment) w/ the current epidemic state
-        @param dt the size of the timestep
-
-        @return deltas to the epidemic state. 
+    def get_deltas(self, current_state: pd.DataFrame, dt: int|float, stochastic: bool) -> pd.DataFrame:
+        """! Method should take in current state and return a series of deltas to that state.
+        @param current_state: a data frame (at the moment) w/ the current epidemic state
+        @param dt: the size of the timestep
+        @return deltas to the epidemic state.
         """
 
         pass
@@ -28,10 +26,10 @@ class Rule(ABC):
     @classmethod
     def from_yaml(cls, rule_yaml):
         '''! This method loads a rule from its full yaml definition. Should not be overridden.
+        @param rule_yaml: a dictionary defining the class(es) read from yaml file
+        @return a version of the class
+        '''
         
-        @param rule_yaml a dictionary defining the class(es) read from yaml file
-        
-        @raturn a version of the class'''
         key = list(rule_yaml.keys())[0]
 
         if '.' in key:
@@ -39,8 +37,7 @@ class Rule(ABC):
             mod = importlib.import_module(mod_nm)
             rule_cls = getattr(mod, cls_nm)
         else:
-            #this ends up being a bit tricky. We need to transverse all
-            #parent frames until we find the key.
+            #need to transverse all parent frames until we find the key.
             rule_cls = inspect.currentframe().f_locals.get(key) #change f_globals to f_locals
             if rule_cls is None:
                 frames = inspect.getouterframes(inspect.currentframe())
@@ -54,17 +51,18 @@ class Rule(ABC):
     @classmethod
     def from_yaml_def(cls, definition):
         '''! This is the method to override to do any special processing of a rule class definition
-         from a yaml file. 
-         
-         @param definition a dictionary giving what is needed. if parameters no need to overide'''
+        from a yaml file. 
+        @param definition: a dictionary giving what is needed. if parameters no need to overide.
+        '''
         
         return(cls(**definition))
 
     @abstractmethod
-    def to_yaml(self):
+    def to_yaml(self) -> dict:
         '''! This method should return dictionary object appropriate for inclusion in a yaml 
         definition of an epidemic. Should be a dictionary with the class name (in form module.classname)
         being the outer key containing information needed for the class to run "from_yaml"
         
-        @return a dictionary representation of this object appropriate to read in by from_yaml'''
+        @return a dictionary representation of this object appropriate to read in by from_yaml
+        '''
         pass
