d0d53ffcbc50a2cb67d382465eb92449cf617a18
Liu <lyue@AD.UNC.EDU>
2025-04-16 15:26:19 -0400

    update filter_data method

--- tabularepimdl/WithFilters.py
+++ tabularepimdl/WithFilters.py
@@ -3,7 +3,7 @@ import pandas as pd
 class WithFilters():
     """Mixin class to add filtering capability to other rules."""
     
-    def __init__(self, filter_column:str = None, filter_value: list[str] =None, *args, **kwargs) -> None: #allows the filter to accept extra arguments for other rules
+    def __init__(self, filter_column:str = None, filter_value: list[str] = None, *args, **kwargs) -> None: #allows the filter to accept extra arguments for other rules
         """
         Initialization.
         @para filter_column: name of the column in dataframe this filter applies to.
@@ -11,10 +11,10 @@ class WithFilters():
         @para *args: other positional arguments this filter may take for other rules.
         @para **kwargs: other keyword arguments this filter may take for other rules.
         """
-        print('start init of withFilters')
+        #print('start init of withFilters')
         self.filter_column = filter_column
         self.filter_value = filter_value
-        print('next go to SimpleTransition to init')
+        #print('next go to SimpleTransition to init')
         super().__init__(*args, **kwargs)
         
     def filter_data(self, current_state: pd.DataFrame) -> pd.DataFrame:
@@ -22,15 +22,20 @@ class WithFilters():
         @para current_state: a dataframe (at the moment) representing the current epidemic state.
         @return: filtered dataframe based on filter values.
         """
+        #print('withfilters filtering data.')
         if self.filter_column is not None and self.filter_value is not None:
-            return(current_state.loc[current_state[self.filter_column] == self.filter_value])
+            return current_state.loc[current_state[self.filter_column].isin(self.filter_value)]
         return current_state
 
-    def filter_get_deltas(self, current_state: pd.DataFrame) -> pd.DataFrame:
+    def get_deltas(self, current_state: pd.DataFrame, dt: int | float = 1.0, stochastic: bool = None) -> pd.DataFrame:
         """Applies the filter before executing the rule logic.
         @para current_state: a dataframe (at the moment) representing the current epidemic state.
         @return: a pandas DataFrame containing changes after executing the rule logic.
         """
+        #print('filter_get_delta')
         filtered_state = self.filter_data(current_state)
-        
-        return super().get_deltas(filtered_state)
+        #print('filter_get_delta returns')
+        return super().get_deltas(filtered_state, dt=dt, stochastic=stochastic)
+    
+    def __str__(self) -> str:
+        return "WithFilter rule."
