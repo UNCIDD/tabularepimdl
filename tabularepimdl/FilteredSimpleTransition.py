49bd093a385f6afa5fc17111de2d13025639f96d
Liu <lyue@AD.UNC.EDU>
2025-04-16 15:28:36 -0400

    update arguments setting

--- tabularepimdl/FilteredSimpleTransition.py
+++ tabularepimdl/FilteredSimpleTransition.py
@@ -3,7 +3,7 @@ from tabularepimdl.WithFilters import WithFilters
 
 class FilteredSimpleTransition(WithFilters, SimpleTransition):
     """A version of SimpleTransition that supports filtering."""
-    def __init__(self, column, from_st, to_st, rate, filter_column=None, filter_value=None, stochastic=False):
+    def __init__(self, column: str, from_st: str, to_st: str, rate: int | float, filter_column: str = None, filter_value: list[str] = None, stochastic=False):
         #initialize the filter and the rule that applies the filter
         super().__init__(filter_column=filter_column, filter_value=filter_value, column=column, from_st=from_st, to_st=to_st, rate=rate, stochastic=stochastic)
        
