e8849627b88ef29269f133ac3f581ecd0484c134
Liu <lyue@AD.UNC.EDU>
2025-04-22 13:09:49 -0400

    redundant code cleanup

--- tabularepimdl/FilteredSimpleTransition.py
+++ tabularepimdl/FilteredSimpleTransition.py
@@ -7,8 +7,6 @@ class FilteredSimpleTransition(WithFilters, SimpleTransition):
     #def __init__(self, column: str, from_st: str, to_st: str, rate: int | float, stochastic: bool = False, filter_column: str = None, filter_value: list[str] = None):
         #initialize the filter and the rule that applies the filter
         #super().__init__(filter_column=filter_column, filter_value=filter_value, column=column, from_st=from_st, to_st=to_st, rate=rate, stochastic=stochastic)
-        #SimpleTransition.__init__(self, column=column, from_st=from_st, to_st=to_st, rate=rate, stochastic=stochastic)
-        #WithFilters.__init__(self, filter_column, filter_value)
-    filter_column: str | None = None #can keep this type hint and it works
-    filter_value: list[str] | None = None #can keep this type hint and it works   
-    #pass
+        
+    filter_column: str | None = None #subclass attribute for filter
+    filter_value: list[str] | None = None #subclass attribute for filter
