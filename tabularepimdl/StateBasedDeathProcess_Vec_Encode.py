2aa1ccd2a27c0ddeaa436cc2011d06ef9817cefc
Liu <lyue@AD.UNC.EDU>
2025-09-23 16:47:44 -0400

    vectorized StateBasedDeathProcess rule

--- tabularepimdl/StateBasedDeathProcess_Vec_Encode.py
+++ tabularepimdl/StateBasedDeathProcess_Vec_Encode.py
@@ -13,7 +13,8 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
 
     Attributes:
         column: single column that we will check states against. Changing columns to column based on the AgingPopulation example.
-        states: the states of single column.
+        all_states: all the states of single column. All the state values are required as input, otherwise the encoding logic cannot assign the correct mapping.
+        target_states: targeted states of single column.
         rate: the rate at whihc people will die from.
         stochastic: whether the process is stochastic or deterministic.
         infstate_compartments: the infection compartments used in epidemics. e.g. ['I', 'R', 'S']
@@ -26,7 +27,8 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
     })
 
     column = 'InfState' #one column only
-    states = ['I', 'S'] #values from single column
+    all_states = ['I', 'R', 'S']
+    target_states = ['I', 'S'] #values from single column
 
     selected_data = pd.DataFrame({
         'N': [10, 30],
@@ -36,7 +38,8 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
     """
 
     column: str = Field(description = "one column that we will check states against.")
-    states: list[str] = Field(description = "the state of single column.")
+    all_states: list[str] = Field(description = "all the states of single column.")
+    target_states: list[str] = Field(description = "targeted states to be processed of single column.")
     rate: float = Field(ge=0, description = "the rate at whihc people will die from.")
     stochastic: bool = Field(default=False, description = "whether the transition is stochastic or deterministic.")
     infstate_compartments: list[str] = Field("the infection compartments used in epidemics.")
@@ -44,7 +47,7 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
     #_columns_code: list[str] | None = PrivateAttr(default_factory=list) #not needed given it is single column
     _states_code: list[str] | None = PrivateAttr(default_factory=list)
 
-    @field_validator("states", mode="before") #validate list type
+    @field_validator("all_states", "target_states", mode="before") #validate list type
     @classmethod
     def validate_list(cls, list_parameters, field: ValidationInfo):
         """Ensure the input is a list."""
@@ -54,9 +57,9 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
     
     def model_post_init(self, _):
         infstate_to_int = {s: i for i, s in enumerate(sorted(self.infstate_compartments))} #for InfState column mapping only
-        states_sorted = sorted(self.states)
+        states_sorted = sorted(self.all_states)
         colstate_to_int = {s: i for i, s in enumerate(states_sorted)} #for the single column mapping
-        self._states_code = [colstate_to_int[state] for state in states_sorted] #encoded column states
+        self._states_code = [colstate_to_int[state] for state in states_sorted if state in self.target_states] #encoded column states
         
         
     def get_deltas(self, current_state: np.ndarray, col_idx_map: dict[str, int], result_buffer: np.ndarray, dt: float =1.0, stochastic: bool | None = None) -> np.ndarray:
@@ -87,10 +90,12 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
         n_idx = col_idx_map['N']
 
         states_mask = np.isin(current_state[:, col_idx], self._states_code) #all rows match single column's states code
+        #print('state mask', states_mask)
 
         #all satisfied records are wanted based on column and state values
         selected_from = current_state[states_mask]
         N = selected_from[:, n_idx]
+        #print('selected from', selected_from)
 
         rate_const = 1 - np.exp(-dt * self.rate)
 
@@ -99,7 +104,8 @@ class StateBasedDeathProcess_Vec_Encode(Rule, BaseModel):
         else:
             changed_N = -N * rate_const
         
-        count = len(states_mask)
+        count = len(selected_from)
+        #print('count:', count)
         
         #Fill selected rows with changed_N values
         result_buffer[:count, :] = selected_from 
