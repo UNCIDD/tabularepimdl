56515e66969d889b3cb1a79f89820d193a648d41
Liu <lyue@AD.UNC.EDU>
2025-10-17 17:18:05 -0400

    benchmark SimpleObservationProcess initial

---
+++ benchmark/SimpleObservationProcessDispatcher.py
@@ -0,0 +1,78 @@
+from pydantic import BaseModel, Field, PrivateAttr
+from typing import Literal, Union
+import pandas as pd
+import numpy as np
+
+from tabularepimdl.SimpleObservationProcess import SimpleObservationProcess as SimpleObservationProcess_Pandas
+from tabularepimdl.SimpleObservationProcess_Vec_Encode import SimpleObservationProcess_Vec_Encode as SimpleObservationProcess_Vec_Encode
+
+class SimpleObservationProcessDispatcher(BaseModel):
+    """
+    Dispatches Pandas and various Numpy versions with Numba feature of SimpleObservationProcess rule at backend.
+    @param structure: data structure used for rules.
+    @param source_col: the column containing source_state for the observation process.
+    @param source_state: the state individuals start, listed in source_col.
+    @param obs_col: the column that contains each group of individuals' observed state.
+    @param rate: the number of people move from a particular state into another state per unit time.
+    @param unobs_state: un-observed state, listed in obs_col.
+    @param incobs_state: incident-observed state, listed in obs_col.
+    @param prevobs_state: previously-observed state, listed in obs_col.
+    @param stochastic: whether the process is stochastic or deterministic.
+    @param infstate_compartments: the infection compartments used in epidemics. E.g.infstate_compartments = ['S', 'I', 'R']. 
+    @param observation_compartments: the observation compartments used in epidemics. e.g. ['U', 'P', 'I'], U=unobserved, P=previously-observed, I=incident-observed
+    """
+
+    structure: Literal["Pandas", "Numpy_Vec_Encode"]
+    source_col: str
+    source_state: str
+    obs_col: str
+    rate: float
+    unobs_state: str
+    incobs_state: str
+    prevobs_state: str
+    stochastic: bool
+    infstate_compartments: list[str]
+    observation_compartments: list[str]
+
+    #Dispatcher
+    _dispatcher: Union[SimpleObservationProcess_Pandas, SimpleObservationProcess_Vec_Encode] = PrivateAttr(default=None)
+
+    def model_post_init(self, _): #initialize dispatcher based on data structures
+        if self.structure == 'Pandas':
+            self._dispatcher = SimpleObservationProcess_Pandas(
+                source_col=self.source_col,
+                source_state=self.source_state,
+                obs_col=self.obs_col,
+                rate=self.rate,
+                unobs_state=self.unobs_state,
+                incobs_state=self.incobs_state,
+                prevobs_state=self.prevobs_state,
+                stochastic=self.stochastic
+            )
+        elif self.structure == 'Numpy_Vec_Encode':
+            self._dispatcher = SimpleObservationProcess_Vec_Encode(
+                source_col=self.source_col,
+                source_state=self.source_state,
+                obs_col=self.obs_col,
+                rate=self.rate,
+                unobs_state=self.unobs_state,
+                incobs_state=self.incobs_state,
+                prevobs_state=self.prevobs_state,
+                stochastic=self.stochastic,
+                infstate_compartments=self.infstate_compartments,
+                observation_compartments=self.observation_compartments
+            )
+        else:
+            raise ValueError(f"Unknown structure: {self.structure}")
+    
+    def get_deltas(self, current_state: pd.DataFrame | np.ndarray, col_idx_map: dict[str, int] | None = None, result_buffer: np.ndarray | None = None,  dt: int | float = 1.0, stochastic: bool | None = None) -> pd.DataFrame | np.ndarray:
+        """
+        @param current_state: a dataframe or numpy array (at the moment) representing the current epidemic state.
+        @param col_idx_map: mapping of input data columns and their column index. Default is None so Pandas version's get_deltas() can invoke dispather's get_deltas().
+        @param result_buffer: takes pre-allocated numpy array and saves changing amount of current_state. Default is None so Pandas version's get_deltas() can invoke dispather's get_deltas().
+        @param dt: size of the timestep.
+        """
+        if self.structure == 'Pandas':
+            return self._dispatcher.get_deltas(current_state=current_state, dt=dt, stochastic=stochastic)
+        elif self.structure == 'Numpy_Vec_Encode':
+            return self._dispatcher.get_deltas(current_state=current_state, col_idx_map=col_idx_map, result_buffer=result_buffer, dt=dt)
---
+++ benchmark/SimpleObservationProcess_Run_n_Visual.py
@@ -0,0 +1,73 @@
+from benchmark.SimpleObservationProcessRunner import SimpleObservationProcessRunner
+from benchmark.Visualizer import Visualizer
+import numpy as np
+import pandas as pd
+import random
+
+compartments = ['S', 'I', 'R']
+observations = ['U', 'P', 'I']
+size = 5 #data size
+
+###===fixed data===###
+"""
+n_values = [10, 30, 50, 20, 5]
+t_values = [2023]*5
+
+current_state = pd.DataFrame({
+    'InfState': ['S', 'S', 'S', 'I', 'R'], #links to source_col
+    'Hosp': ['U', 'P', 'I', 'U', 'P'], #links to obs_col 
+    'N': n_values,
+    'T': t_values
+})
+"""
+
+###===random data===###
+
+#n_samples = size
+#random_indices = np.random.randint(0, size, size=n_samples)
+#random_cats = [f'catg_{i}' for i in random_indices]
+#print('random cats:', random_cats) #debug
+
+inf_state = [random.choice(compartments) for _ in range(size)] #inf states
+hosp_state = [random.choice(observations) for _ in range(size)] #inf states
+n_values = [random.randint(10, 1_000_000) for _ in range(size)]
+t_values = [2023]*size
+
+current_state = pd.DataFrame({
+                'InfState' : inf_state,
+                'Hosp': hosp_state,
+                'N' : n_values,
+                'T' : t_values
+                })
+
+current_state = current_state.groupby(['InfState', 'Hosp'], observed=True).agg({'N': 'sum', 'T': 'max'}).reset_index()
+print('current_state\n', current_state)
+
+def SimpleObservation_Run_n_Visual():
+    runner = SimpleObservationProcessRunner(
+        data_sizes= [size],
+        data_input = current_state,
+        structures= ["Pandas", "Numpy_Vec_Encode"],
+        iterations= [1],#[100, 300, 500, 700],
+        
+        source_col = "InfState",
+        source_state = "I",
+        obs_col = 'Hosp',
+        rate = 0.05,
+        unobs_state = 'U',
+        incobs_state = 'I',
+        prevobs_state = 'P',
+        
+        stochastic=False,
+        col_idx_map = {'InfState': 0, 'Hosp': 1, 'N': 2, 'T': 3},
+        infstate_compartments = compartments,
+        observation_compartments = observations
+    )
+    results = runner.run()
+
+    viz = Visualizer(runner_results = results)
+    viz.plot()
+
+if __name__ == "__main__":
+    SimpleObservation_Run_n_Visual()
+#python -m benchmark.SimpleObservationProcess_Run_n_Visual
