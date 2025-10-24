bc3c459bf7ba03bc93dbd86e3f5ca3ed604930b9
Liu <lyue@AD.UNC.EDU>
2025-10-24 15:21:30 -0400

    add rule property for categorical columns

--- tabularepimdl/SimpleInfection_Vec_Encode.py
+++ tabularepimdl/SimpleInfection_Vec_Encode.py
@@ -124,10 +124,7 @@ class SimpleInfection_Vec_Encode(Rule, BaseModel):
         }
         return rc
     
+    #set up a property to return all the required compartments used in infstate column
     @property
-    def source_states(self):
-        return [self.s_st]
-
-    @property
-    def target_states(self):
-        return [self.inf_to]
+    def infstate_all(self) -> list[str]: 
+        return self.infstate_compartments
