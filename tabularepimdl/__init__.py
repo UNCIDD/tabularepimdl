fe177d3df28ccf93f3bfd5d0a12a5842e0a4064a
J.C. Macdonald <72512262+jc-macdonald@users.noreply.github.com>
2026-04-30 10:13:22 -0400

    Address PR #70 review: move experimental rules/tests, clean __init__
    Per bestill3 review:
    - Move to scripts/experimental_rules/: FilteredSimpleInfection,
      FilteredSimpleTransition, SharedTraitInfection_MultiBeta,
      SharedTraitInfectionValueFilter, WAIFWTransmission_Vec_Encode_Numba,
      WithFilters
    - Move epitest/benchmark_EpiModel_Pandas_Vec_test.py to
      scripts/experimental_engines/ (outdated)
    - Remove epitest/rule_test.ipynb (duplicate)
    - Update __init__.py: remove moved imports, add comment for Numba rule
    
    Vec_Encode engines remain in scripts/experimental_engines/ (not
    promoted to tabularepimdl/ per maintainer decision).

--- tabularepimdl/__init__.py
+++ tabularepimdl/__init__.py
@@ -3,17 +3,13 @@ from .BirthProcess import BirthProcess
 from .EnvironmentalTransmission import EnvironmentalTransmission
 from .EpiModel import EpiModel
 from .EpiRunner import EpiRunner
-from .FilteredSimpleInfection import FilteredSimpleInfection
-from .FilteredSimpleTransition import FilteredSimpleTransition
 from .MultiStrainInfectiousProcess import MultiStrainInfectiousProcess
 from .SharedTraitInfection import SharedTraitInfection
-from .SharedTraitInfectionValueFilter import SharedTraitInfectionValueFilter
 from .SimpleInfection import SimpleInfection
 from .SimpleObservationProcess import SimpleObservationProcess
 from .SimpleTransition import SimpleTransition
 from .StateBasedDeathProcess import StateBasedDeathProcess
 from .WAIFWTransmission import WAIFWTransmission
-from .WithFilters import WithFilters
 from .FunctionalTransition import FunctionalTransition
 
 ####The following rules are in vectorized/numpy structure#########
@@ -24,7 +20,11 @@ from .SimpleObservationProcess_Vec_Encode import SimpleObservationProcess_Vec_En
 from .StateBasedDeathProcess_Vec_Encode import StateBasedDeathProcess_Vec_Encode
 from .WAIFWTransmission_Vec_Encode_Bincount import WAIFWTransmission_Vec_Encode_Bincount
 
-from .EpiModel_Vec_Encode2 import EpiModel_Vec_Encode_2
-from .EpiModel_Vec_Encode1 import EpiModel_Vec_Encode_1
-from .EpiModel_Vec_Encode1_2 import EpiModel_Vec_Encode_1_2
+# Moved to scripts/experimental_rules/ (less efficient than Bincount; see issue #52):
+# from .WAIFWTransmission_Vec_Encode_Numba import WAIFWTransmission_Vec_Encode_Numba
+
+# Experimental engine versions moved to scripts/experimental_engines/
+# from .EpiModel_Vec_Encode2 import EpiModel_Vec_Encode_2
+# from .EpiModel_Vec_Encode1 import EpiModel_Vec_Encode_1
+# from .EpiModel_Vec_Encode1_2 import EpiModel_Vec_Encode_1_2
 __version__ = "0.2.0"
