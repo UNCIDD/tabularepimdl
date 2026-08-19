6bd39009212ebc8c3ac9dbca27feaafa2243ffc8
Yue Liu <146049182+bestill3@users.noreply.github.com>
2026-08-19 16:19:17 -0400

    lint warning fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 from pathlib import Path
 
-import pytest
-import pandas as pd
 import numpy as np
+import pandas as pd
+import pytest
 
 """
 confest.py provides fixtures for the entire directory.
