8d877b90e3249082cf4de476ee79361bbf467ce4
Yue Liu <146049182+bestill3@users.noreply.github.com>
2026-08-19 15:12:10 -0400

    lint warning fix

--- src/tabularepimdl/SimpleTransition_Vec_Encode.py
+++ src/tabularepimdl/SimpleTransition_Vec_Encode.py
@@ -1,10 +1,11 @@
 import logging
+
 import numpy as np
 from pydantic import BaseModel, Field, PrivateAttr
 
-from tabularepimdl.Rule import Rule
 from tabularepimdl._types.constrained_types import UniqueNonEmptyStrList
 from tabularepimdl._validators.rule_domain_membership_validator import domain_membership_validator
+from tabularepimdl.Rule import Rule
 
 logger = logging.getLogger(__name__)
 
